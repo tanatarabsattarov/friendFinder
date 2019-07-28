619f719557479ff99203b31a5ce17d7e146141b3
Tanatar Absattarov <Tanatar@Tanatars-MacBook-Air.local>
2019-07-28 16:46:15 -0400

    final commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
     {
         "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/%D0%90%D1%85%D0%BC%D0%B5%D1%82_%D0%91%D0%B0%D0%B9%D1%82%D2%B1%D1%80%D1%81%D1%8B%D0%BD%D0%BE%D0%B2.jpg/230px-%D0%90%D1%85%D0%BC%D0%B5%D1%82_%D0%91%D0%B0%D0%B9%D1%82%D2%B1%D1%80%D1%81%D1%8B%D0%BD%D0%BE%D0%B2.jpg",
         "scores": [
             5,
             1,
